fd59c13a6abb251f003bfd6ff38e97984ef6568d
German Vargas <german.vargas@episource.com>
2024-06-26 13:50:14 -0500

    bare bones reactjs app with material ui support

---
+++ mui-tutorial/src/App.jsx
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Test from "./Test";
+
+const App = () => {
+  return <Test />;
+};
+
+export default App;
---
+++ mui-tutorial/src/Test.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const Test = () => {
+  return (
+    <Typography variant="h1" sx={{ color: red }}>
+      Hello World
+    </Typography>
+  );
+};
+
+export default Test;
